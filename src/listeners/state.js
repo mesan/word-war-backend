2ec76d3fad8096fa10c3f7ae3a6df90f80a36bdb
Arild Wanvik Tvergrov <arildt@mesan.no>
2014-10-04 17:08:45 +0200

    Log when state is requested.

--- src/listeners/state.js
+++ src/listeners/state.js
@@ -1,5 +1,8 @@
 module.exports.listen = function (socket, wordWar) {
   socket.on('state', function () {
+
+    console.log(socket.id + ': State requested...');
+
     socket.emit('currentState', {
       users: wordWar.users,
       letters: wordWar.service.getCurrentLetters()
